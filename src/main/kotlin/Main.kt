f984720cf1021bbcac909a69b337b686bb0c177d
Sergey Makletsov <smaklets@kpfu.ru>
2023-10-11 11:44:27 +0300

    Рисование

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -7,19 +10,46 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import drawing.CartesianPainter
+import kotlin.random.Random
+
+object Params{
+    val _color
+        get() = mutableStateOf(Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat()))
+
+    val color: Color
+        get() = _color.value
+}
 
 @Composable
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
-
+    val p = CartesianPainter()
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+
+        Column() {
+            Box(Modifier.fillMaxWidth().fillMaxHeight(0.9f)) {
+                Canvas(Modifier.fillMaxSize()) {
+                    p.paint(this)
+                }
+            }
+            Box(Modifier.background(Color.Gray).fillMaxWidth()) {
+                Column(Modifier.fillMaxWidth()) {
+                    Button(onClick = {
+                        //Params.color=Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat())
+                    }, Modifier.align(Alignment.CenterHorizontally)) {
+                        Text(text)
+                    }
+                }
+            }
         }
     }
 }
--- src/main/kotlin/drawing/CartesianPainter.kt
+++ src/main/kotlin/drawing/CartesianPainter.kt
@@ -1,4 +1,14 @@
 package drawing
 
-class CartesianPainter {
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+
+open class CartesianPainter {
+
+    fun paint(scope: DrawScope){
+        scope.apply{
+            drawLine(Color.Black, Offset(0f, size.height/2), Offset(size.width, size.height/2), 3f)
+        }
+    }
 }
